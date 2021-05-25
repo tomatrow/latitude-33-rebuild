d39bcb399f8fa4065a9629df0a1602881e19d570
AJ Caldwell <tomatrow@gmail.com>
2021-05-25 10:13:50 -0700

    Add netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import format from "@tomatrow/zen-format"
 import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-netlify"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,6 +15,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+        adapter: adapter(),
         vite: {
             plugins: [
                 format({ load: true })
