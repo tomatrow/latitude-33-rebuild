454335a41db7ca7e629a7eff00bcd83ebf60e2a3
AJ Caldwell <tomatrow@gmail.com>
2021-05-25 09:59:39 -0700

    Add services to home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-    import Link from "$lib/components/Link.svelte"
-    import Button from "$lib/components/Button.svelte"
-    import YouTubeModal from "$lib/components/YouTubeModal.svelte"
-    import { openModal } from "$lib/components/ModalProvider.svelte"
     import Hero from "./_Hero.svelte"
     import Grid from "./_Grid.svelte"
     import Why from "./_Why.svelte"
     import TriBlurbs from "./_TriBlurbs.svelte"
     import Offerings from "./_Offerings.svelte"
+    import Services from "./_Services.svelte"
 </script>
 
 <Hero />
@@ -15,3 +12,4 @@
 <Why />
 <TriBlurbs />
 <Offerings />
+<Services />
