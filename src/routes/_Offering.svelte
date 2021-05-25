620868c17c0b47dca97fc497e556b2e695dc6738
AJ Caldwell <tomatrow@gmail.com>
2021-05-25 14:29:01 -0700

    Mobilize offerines

--- src/routes/_Offering.svelte
+++ src/routes/_Offering.svelte
@@ -6,16 +6,26 @@
     export let contentHtml: string
     export let linkTitle: string
     export let href: string
+    export let imageUrl: string
+    export let imageAlt: string
 </script>
 
-<div class="offering pl-14 pt-24 pb-12">
-    <h5 class="font-display text-either-gray-blue font-bold text-xl">{subheading}</h5>
-    <h4 class="font-display mt-2 mb-6 pr-20 font-black text-5xl">{heading}</h4>
-    <div class="injected-content space-y-10 mb-4 tracking-wider font-light text-xl">
+<img
+    class="offering-raise transition-transform transition-shadow duration-200 transform hover:-translate-y-3 rounded-xl z-10 mx-auto w-full max-w-lg"
+    src={imageUrl}
+    alt={imageAlt}
+/>
+
+<div class="offering md:pl-14 pt-12 md:pt-24 pb-12">
+    <h5 class="font-display text-either-gray-blue font-bold md:text-xl">{subheading}</h5>
+    <h4 class="font-display mt-2 mb-6 md:pr-20 font-black text-2xl md:text-5xl">
+        {heading}
+    </h4>
+    <div class="injected-content space-y-10 mb-4 tracking-wider font-light md:text-xl">
         {@html contentHtml}
     </div>
     <Link
-        class="action-link transition duration-200 font-display bg-pre-coffee-sky-blue py-4 px-6 rounded text-white font-black text-sm"
+        class="action-link transition duration-200 font-display bg-pre-coffee-sky-blue inline-block py-4 px-6 rounded text-white font-black text-sm"
         {href}>{linkTitle}</Link
     >
 </div>
@@ -29,5 +39,9 @@
         a.action-link:hover {
             box-shadow: inset 0 200px 200px 200px rgba(0, 0, 0, 0.1);
         }
+    }
+
+    .offering-raise:hover {
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.1);
     }
 </style>
