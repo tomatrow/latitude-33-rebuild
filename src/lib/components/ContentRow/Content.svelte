d9b014e3347c1147aeab24ef98cc043a3a474668
AJ Caldwell <tomatrow@gmail.com>
2021-10-04 21:17:02 -0700

    Add a component for injected content

---
+++ src/lib/components/ContentRow/Content.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let clazz = ""
+    export { clazz as class }
+    export let contentHtml: string
+</script>
+
+{#if contentHtml}
+    <div class="injected-content {clazz}">
+        {@html contentHtml}
+    </div>
+{/if}
